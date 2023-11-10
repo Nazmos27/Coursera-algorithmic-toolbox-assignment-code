42f12a73ee44e325bc02cf5dbec9738d17a157b6
Nazmos27 <nazmos789@gmail.com>
2023-11-11 00:36:31 +0600

    problem 3 solved

---
+++ GCD03.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  long a;
+  long b;
+  cin >> a;
+  cin >> b;
+
+  while(b!=0){
+    long temp = b;
+    b = a % b;
+    a = temp;
+  }
+
+  cout << a;
+}
