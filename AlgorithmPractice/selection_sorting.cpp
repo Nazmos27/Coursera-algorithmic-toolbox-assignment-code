25009fda2a3452978135a1e61451e498e91aedab
Nazmos27 <nazmos789@gmail.com>
2023-11-18 13:39:31 +0600

    selection sort completed

--- AlgorithmPractice/selection_sorting.cpp
+++ AlgorithmPractice/selection_sorting.cpp
@@ -3,14 +3,25 @@ using namespace std;
 
 int main(){
   int a[8]={5,7,3,5,1,10,14,6};
-  int minValue = a[0];
-  for (int i = 0; i< 8; ++i ){
-    if(minValue>a[i]){
-      minValue = a[i];
+  int minIndex = 0;
+  for(int i = 0; i< 7; ++i){
+    minIndex = i;
+    for(int x = i+1; x<8;++x){
+      if(a[x]<a[minIndex]){
+        minIndex = x;
+      }
+    }
+    if(minIndex!=i){
+      swap(a[minIndex],a[i]);
     }
   }
+
+
+
+
   for(int j = 0;j<8;++j){
-    cout << a[j] << ",";
+    cout <<endl<< a[j] << ",";
   }
-  cout<<endl <<"min value is: "<< minValue;
+
+  return 0;
 }
