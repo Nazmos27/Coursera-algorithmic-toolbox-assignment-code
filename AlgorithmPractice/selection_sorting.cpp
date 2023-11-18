9af67d7d75bad9793c510d48c5cb1fb6bc52d517
Nazmos27 <nazmos789@gmail.com>
2023-11-18 13:58:34 +0600

    modified code with the help of chatGPT

--- AlgorithmPractice/selection_sorting.cpp
+++ AlgorithmPractice/selection_sorting.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
+#include <sstream>
+
 using namespace std;
 
-int main(){
-  int a[8]={5,7,3,5,1,10,14,6};
+
+void selectionSort(int a[],int n){
   int minIndex = 0;
-  for(int i = 0; i< 7; ++i){
+  for(int i = 0; i< n-1; ++i){
     minIndex = i;
-    for(int x = i+1; x<8;++x){
+    for(int x = i+1; x<n;++x){
       if(a[x]<a[minIndex]){
         minIndex = x;
       }
@@ -15,13 +17,53 @@ int main(){
       swap(a[minIndex],a[i]);
     }
   }
+}
 
+// Function to print an array
+void printArray(int a[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
 
+int main(){
 
+    string input;
+    cout << "Enter numbers separated by spaces: ";
+    getline(cin, input);
 
-  for(int j = 0;j<8;++j){
-    cout <<endl<< a[j] << ",";
-  }
+    // Use a stringstream to extract integers from the input
+    istringstream iss(input);
+    int number;
+    int count = 0;
 
-  return 0;
+    // Count the number of elements
+    while (iss >> number) {
+        count++;
+    }
+
+    // Allocate an array of the required size
+    int* a = new int[count];
+
+    // Reset stringstream to read the numbers again
+    iss.clear();
+    iss.seekg(0);
+
+    // Read the numbers into the array
+    for (int i = 0; i < count; i++) {
+        iss >> a[i];
+    }
+
+    // Function Call
+    selectionSort(a, count);
+    cout << "Sorted array: \n";
+    printArray(a, count);
+
+    // Deallocate the dynamically allocated memory
+    delete[] a;
+
+    return 0;
 }
