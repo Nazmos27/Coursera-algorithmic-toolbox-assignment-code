bbf32842decc5133e038202f6d370bedda85eeb2
Nazmos27 <nazmos789@gmail.com>
2023-11-23 23:34:53 +0600

    testing

--- AlgorithmPractice/merge_sorting.cpp
+++ AlgorithmPractice/merge_sorting.cpp
@@ -103,3 +103,41 @@ int main()
 // This code is contributed by Mayank Tyagi
 // This code was revised by Joshua Estes
 
+int main2(){
+  string input;
+    cout << "Enter numbers separated by spaces: ";
+    getline(cin, input);
+
+    // Use a stringstream to extract integers from the input
+    istringstream iss(input);
+    int number;
+    int count = 0;
+
+    // Count the number of elements
+    while (iss >> number) {
+        count++;
+    }
+
+    // Allocate an array of the required size
+    int* a = new int[count];
+
+    // Reset stringstream to read the numbers again
+    iss.clear();
+    iss.seekg(0);
+
+    // Read the numbers into the array
+    for (int i = 0; i < count; i++) {
+        iss >> a[i];
+    }
+
+    // Function Call
+    selectionSort(a, count);
+    cout << "Sorted array: \n";
+    printArray(a, count);
+
+    // Deallocate the dynamically allocated memory
+    delete[] a;
+
+    return 0;
+}
+
