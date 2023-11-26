40a88a30a983f1e95a1415372ae703c9a89daa6a
Nazmos27 <nazmos789@gmail.com>
2023-11-26 23:28:00 +0600

    quick sort algorithm

--- AlgorithmPractice/quick_sort.cpp
+++ AlgorithmPractice/quick_sort.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int partition(int arr[],int low,int high)
+{
+//choose the pivot
+
+int pivot=arr[high];
+//Index of smaller element and Indicate
+//the right position of pivot found so far
+int i=(low-1);
+
+for(int j=low;j<=high;j++)
+{
+	//If current element is smaller than the pivot
+	if(arr[j]<pivot)
+	{
+	//Increment index of smaller element
+	i++;
+	swap(arr[i],arr[j]);
+	}
+}
+swap(arr[i+1],arr[high]);
+return (i+1);
+}
+
+
 
 
 int main() {
